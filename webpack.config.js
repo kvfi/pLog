31dede44d5277a58b46fb97173bbd0bc627d16bc
kvfi <kouafi@icloud.com>
2017-03-21 18:25:04 +0000

    Add context and module loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 var path = require('path');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/assets/dist')
-  }
+	context: path.join(__dirname, 'src'),
+	entry: './index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public/assets/dist')
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: ['babel-loader'],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				loader: ['style-loader', 'css-loader']
+			}
+		]
+	}
 };
