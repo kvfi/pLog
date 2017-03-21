07b9613ea14a382a70ca7a4f6f197f962791afa9
kvfi <kouafi@icloud.com>
2017-03-21 17:53:38 +0000

    Move index.js from root dir to src dir

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
+import "cardinalcss/cardinal.css";
 
 function component () {
   var element = document.createElement('div');
 
   /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.innerHTML = _.join(['Hello','webpack', 'mdr', 'xd'], ' ');
 
   return element;
 }
