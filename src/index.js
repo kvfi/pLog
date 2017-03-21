0c13bb2bca3cab184d7d43f0cbaeb77d6d0cbbf9
kvfi <kouafi@icloud.com>
2017-03-21 18:35:01 +0000

    Remove CardinalCSS

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import _ from 'lodash';
-import "cardinalcss/cardinal.css";
 
-function component () {
-  var element = document.createElement('div');
+function component() {
+    var element = document.createElement('div');
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack', 'mdr', 'xd'], ' ');
+    /* lodash is required for the next line to work */
+    element.innerHTML = _.join(['Hello', 'webpack', 'mdr', 'xd'], ' ');
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
